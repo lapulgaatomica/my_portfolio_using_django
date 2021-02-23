9fbbc688d5ca64f53dcd96c1a5a78b06ed833482
Odedoyin Akindele <akindeleodedoyin@gmail.com>
2021-02-23 07:59:13 +0100

    added tests for '/skill/new' and 'aboutme/new' routes and views

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,7 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from .views import HomePageView
+from .views import HomePageView, NewAboutView, NewSkillView
 
 class HomepageTests(TestCase):
 
@@ -29,6 +30,8 @@ class HomepageTests(TestCase):
             HomePageView.as_view().__name__
         )
 
+    # add tests that ascertain that there is "add new "about me"" and "add new skill" buttons on the homepage when superuser is logged in
+
 class NewAboutViewTestsForNormalUsers(TestCase):
 
     def setUp(self):
@@ -38,6 +41,35 @@ class NewAboutViewTestsForNormalUsers(TestCase):
     def test_about_view_status_code_for_non_super_user(self):
         self.assertEqual(self.response.status_code, 302)
 
+class NewAboutViewTestsForSuperUsers(TestCase):
+
+    def setUp(self):
+        self.super_user = get_user_model().objects.create_superuser(
+            username='delesuper',
+            email='dele@super.com',
+            password='password'
+        )
+        self.client.login(username='delesuper', password='password')
+        url = reverse('new_about')
+        self.response = self.client.get(url)
+
+    def test_new_about_view_status_code_for_super_user(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_new_about_template_used(self):
+        self.assertTemplateUsed(self.response, 'new_aboutme.html')
+
+    def test_new_about_contains_correct_html(self):
+        self.assertContains(self.response, 'Paragraph')
+        self.assertContains(self.response, 'Add New "About Me"')
+        self.assertContains(self.response, 'Submit')
+
+    def test_new_about_url_resolves_homepageview(self):
+        view = resolve('/aboutme/new')
+        self.assertEqual(
+            view.func.__name__,
+            NewAboutView.as_view().__name__
+        )
 
 class NewSkillViewTestsForNormalUsers(TestCase):
 
@@ -48,4 +80,36 @@ class NewSkillViewTestsForNormalUsers(TestCase):
     def test_skill_view_status_code_for_non_super_user(self):
         self.assertEqual(self.response.status_code, 302)
 
-#Reminder to create an newaboutview and newskillview tests for a superuser
+class NewSkillViewTestsForSuperUsers(TestCase):
+
+    def setUp(self):
+        self.super_user = get_user_model().objects.create_superuser(
+            username='delesuper',
+            email='dele@super.com',
+            password='password'
+        )
+        self.client.login(username='delesuper', password='password')
+        url = reverse('new_skill')
+        self.response = self.client.get(url)
+
+    def test_new_skill_view_status_code_for_super_user(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_new_skill_template_used(self):
+        self.assertTemplateUsed(self.response, 'new_skill.html')
+
+    def test_new_skill_contains_correct_html(self):
+        self.assertContains(self.response, 'Add New Skill')
+        self.assertContains(self.response, 'Skill')
+        self.assertContains(self.response, 'Add Skill')
+
+    def test_new_skill_url_resolves_homepageview(self):
+        view = resolve('/skill/new')
+        self.assertEqual(
+            view.func.__name__,
+            NewSkillView.as_view().__name__
+        )
+
+# Reminder to create a newaboutview and newskillview tests for a superuser
+# Reminder to create a edit and delete aboutview and skillview tests for a superuser
+# Reminder to test models
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('aboutme/<int:pk>/edit', UpdateAboutView.as_view(), name='edit_about'),
     path('aboutme/<int:pk>/delete', DeleteAboutView.as_view(), name='delete_about'),
     path('skill/new', NewSkillView.as_view(), name='new_skill'),
-    path('skill/<int:pk>/edit', UpdateSkillView.as_view(), name='edit_competency'),
-    path('skill/<int:pk>/delete', DeleteSkillView.as_view(), name='delete_competency'),
+    path('skill/<int:pk>/edit', UpdateSkillView.as_view(), name='edit_skill'),
+    path('skill/<int:pk>/delete', DeleteSkillView.as_view(), name='delete_skill'),
 ]
