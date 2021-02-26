a04ed77498dc7779d817ed84a49442fb12d0933d
pulga <akindeleodedoyin@gmail.com>
2021-02-26 11:18:58 +0100

    added tests for UpdateReasonView and DeleteReasonView and their urls and the Reason model, implicitly

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -455,5 +455,115 @@ class NewReasonViewTestsForSuperUsers(TestCase):
         self.assertContains(self.post_response, 'Edit')
         self.assertContains(self.post_response, 'Delete')
 
+class UpdateReasonViewTestsForNormalUsers(TestCase):
+
+    def setUp(self):
+        self.reason = Reason.objects.create(
+            purpose='I want to hire you'
+        )
+        url = reverse('edit_reason', args=[str(self.reason.id)])
+        self.response = self.client.get(url)
+
+    def test_update_reason_view_status_code_for_non_super_user(self):
+        self.assertEqual(self.response.status_code, 302)
+
+class UpdateReasonViewTestsForSuperUsers(TestCase):
+
+    def setUp(self):
+        get_user_model().objects.create_superuser(
+            username='delesuper',
+            email='dele@super.com',
+            password='password'
+        )
+        self.client.login(username='delesuper', password='password')
+        self.reason = Reason.objects.create(
+            purpose='I want to hire you'
+        )
+        url = reverse('edit_reason', args=[str(self.reason.id)])
+        self.response = self.client.get(url)
+        self.post_response = self.client.post(url, {'purpose':'Want to hire you'}, follow=True)
+
+    def test_update_reason_view_status_code_for_super_user(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_update_reason_template_used(self):
+        self.assertTemplateUsed(self.response, 'update_reason.html')
+
+    def test_update_reason_contains_correct_html(self):
+        self.assertContains(self.response,
+         'Edit "I want to hire you"')
+
+    def test_update_reason_url_resolves_update_skill_view(self):
+        view = resolve(f'/reasons/{self.reason.id}/edit')
+        self.assertEqual(
+            view.func.__name__,
+            UpdateReasonView.as_view().__name__
+        )
+
+    def test_status_code(self):
+        self.assertEqual(self.post_response.status_code, 200)
+
+    def test_template_used(self):
+        self.assertTemplateUsed(self.post_response, 'reason.html')
+
+    def test_contains_correct_text(self):
+        self.assertContains(self.post_response, 'Want to hire you')
+
+    def test_does_not_contain_incorrect_text(self):
+        self.assertNotContains(self.post_response, 'I want to hire you')
+
+class DeleteReasonViewTestsForNormalUsers(TestCase):
+
+    def setUp(self):
+        self.reason = Reason.objects.create(
+            purpose='I want to hire you'
+        )
+        url = reverse('delete_reason', args=[str(self.reason.id)])
+        self.response = self.client.get(url)
+
+    def test_delete_reason_view_status_code_for_non_super_user(self):
+        self.assertEqual(self.response.status_code, 302)
+
+class DeleteReasonViewTestsForSuperUsers(TestCase):
+
+    def setUp(self):
+        get_user_model().objects.create_superuser(
+            username='delesuper',
+            email='dele@super.com',
+            password='password'
+        )
+        self.client.login(username='delesuper', password='password')
+        self.reason = Reason.objects.create(
+            purpose='I want to hire you'
+        )
+        url = reverse('delete_reason', args=[str(self.reason.id)])
+        self.response = self.client.get(url)
+        self.post_response = self.client.post(url, follow=True)
+
+    def test_delete_reason_view_status_code_for_super_user(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_delete_reason_template_used(self):
+        self.assertTemplateUsed(self.response, 'delete_reason.html')
+
+    def test_delete_reason_contains_correct_html(self):
+        self.assertContains(self.response, 'Delete "I want to hire you"')
+        self.assertContains(self.response, 'Are you sure you want to delete "I want to hire you"?')
+
+    def test_delete_reason_url_resolves_update_skill_view(self):
+        view = resolve(f'/reasons/{self.reason.id}/delete')
+        self.assertEqual(
+            view.func.__name__,
+            DeleteReasonView.as_view().__name__
+        )
+
+    def test_post_status_code(self):
+        self.assertEqual(self.post_response.status_code, 200)
+
+    def test_post_template_used(self):
+        self.assertTemplateUsed(self.post_response, 'reason.html')
+
+    def test_post_contains_correct_text(self):
+        self.assertNotContains(self.post_response, 'I want to hire you')
 
 # Reminder to follow the DRY principle and stop writing the exact same login code in every testclass
